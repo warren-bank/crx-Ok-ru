5d0647637313944745d301d811ff0dbfcdaf68ae
Warren R. Bank <warren.r.bank@gmail.com>
2020-04-28 11:52:07 -0700

    v0.2.0
    * add an additional alternate video player option: "ExoAirPlayer"
    * add a fallback strategy to handle cross-origin permission errors

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ### [Ok.ru](https://github.com/warren-bank/crx-Ok-ru)
 
-Transfer embedded video stream from [Ok.ru website](https://ok.ru/) to player on [WebCast-Reloaded](https://github.com/warren-bank/crx-webcast-reloaded) [external website](https://warren-bank.github.io/crx-webcast-reloaded/external_website/index.html).
+Transfer embedded video stream from [Ok.ru website](https://ok.ru/) to either:
+* an HTML5 video player on the [WebCast-Reloaded](https://github.com/warren-bank/crx-webcast-reloaded) [external website](https://warren-bank.github.io/crx-webcast-reloaded/external_website/index.html)
+* an instance of [ExoAirPlayer](https://github.com/warren-bank/Android-ExoPlayer-AirPlay-Receiver/releases)
 
 #### Summary:
 
@@ -11,7 +13,7 @@ Transfer embedded video stream from [Ok.ru website](https://ok.ru/) to player on
 
 * video streams __will not__ play directly in the browser window
   - lack of permissive CORS HTTP response headers
-* video streams __will__ play directly on Chromecast
+* video streams __will__ play directly on Chromecast and ExoAirPlayer
 
 #### Legal:
 
--- greasemonkey-userscript/Ok-ru.user.js
+++ greasemonkey-userscript/Ok-ru.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Ok.ru
 // @description  Transfer video stream to player on WebCast-Reloaded external website.
-// @version      0.1.1
+// @version      0.2.0
+// @match        *://ok.ru/videoembed/*
 // @match        *://*.ok.ru/videoembed/*
 // @match        *://href.li/?https://ok.ru/videoembed/*
 // @match        *://href.li/?https://www.ok.ru/videoembed/*
 // @icon         https://ok.ru/favicon.ico
 // @run-at       document-idle
-// @homepage     https://github.com/warren-bank/crx-Ok-ru
+// @homepage     https://github.com/warren-bank/crx-Ok-ru/tree/greasemonkey-userscript
 // @supportURL   https://github.com/warren-bank/crx-Ok-ru/issues
 // @downloadURL  https://github.com/warren-bank/crx-Ok-ru/raw/greasemonkey-userscript/greasemonkey-userscript/Ok-ru.user.js
 // @updateURL    https://github.com/warren-bank/crx-Ok-ru/raw/greasemonkey-userscript/greasemonkey-userscript/Ok-ru.user.js
@@ -18,21 +19,60 @@
 
 // https://www.chromium.org/developers/design-documents/user-scripts
 
+var user_options = {
+  "script_injection_delay_ms":   0,
+  "open_in_webcast_reloaded":    false,
+  "open_in_exoairplayer_sender": true
+}
+
 var payload = function(){
+  const get_referer = () => {
+    let url = ''
+    try {
+      url = top.location.href
+    }
+    catch(e){
+      url = window.location.href
+    }
+    return url
+  }
+
+  const redirect_url = (url) => {
+    try {
+      top.location = url
+    }
+    catch(e){
+      window.location = url
+    }
+  }
+
   const process_video_url = (hls_url) => {
     let encoded_hls_url, webcast_reloaded_base, webcast_reloaded_url
+    let encoded_referer_url, exoairplayer_base, exoairplayer_url
 
     encoded_hls_url       = encodeURIComponent(encodeURIComponent(btoa(hls_url)))
     webcast_reloaded_base = {
-      "https": "https://warren-bank.github.io/crx-webcast-reloaded/external_website/index.html#/watch/",
-      "http":  "http://gitcdn.link/cdn/warren-bank/crx-webcast-reloaded/gh-pages/external_website/index.html#/watch/"
+      "https": "https://warren-bank.github.io/crx-webcast-reloaded/external_website/index.html",
+      "http":  "http://webcast-reloaded.surge.sh/index.html"
     }
     webcast_reloaded_base = (hls_url.toLowerCase().indexOf('https:') === 0)
                               ? webcast_reloaded_base.https
                               : webcast_reloaded_base.http
-    webcast_reloaded_url  = webcast_reloaded_base + encoded_hls_url
+    webcast_reloaded_url  = webcast_reloaded_base + '#/watch/' + encoded_hls_url
 
-    top.location = webcast_reloaded_url
+    encoded_referer_url   = encodeURIComponent(encodeURIComponent(btoa(get_referer())))
+    exoairplayer_base     = 'http://webcast-reloaded.surge.sh/airplay_sender.html'
+    exoairplayer_url      = exoairplayer_base  + '#/watch/' + encoded_hls_url + '/referer/' + encoded_referer_url
+
+    if (window.open_in_webcast_reloaded && webcast_reloaded_url) {
+      redirect_url(webcast_reloaded_url)
+      return
+    }
+
+    if (window.open_in_exoairplayer_sender && exoairplayer_url) {
+      redirect_url(exoairplayer_url)
+      return
+    }
   }
 
   const process_data = (data) => {
@@ -55,27 +95,52 @@ var payload = function(){
   process_page()
 }
 
-var inject_payload = function(){
+var get_hash_code = function(str){
+  var hash, i, char
+  hash = 0
+  if (str.length == 0) {
+    return hash
+  }
+  for (i = 0; i < str.length; i++) {
+    char = str.charCodeAt(i)
+    hash = ((hash<<5)-hash)+char
+    hash = hash & hash  // Convert to 32bit integer
+  }
+  return Math.abs(hash)
+}
+
+var inject_function = function(_function){
   var inline, script, head
 
-  inline = document.createTextNode(
-    '(' + payload.toString() + ')()'
-  )
+  inline = _function.toString()
+  inline = '(' + inline + ')()' + '; //# sourceURL=crx_extension.' + get_hash_code(inline)
+  inline = document.createTextNode(inline)
 
   script = document.createElement('script')
   script.appendChild(inline)
 
-  head = document.getElementsByTagName('head')[0]
+  head = document.head
   head.appendChild(script)
 }
 
-if (document.readyState === 'complete'){
-  inject_payload()
+var inject_options = function(){
+  var _function = `function(){
+    window.open_in_webcast_reloaded    = ${user_options['open_in_webcast_reloaded']}
+    window.open_in_exoairplayer_sender = ${user_options['open_in_exoairplayer_sender']}
+  }`
+  inject_function(_function)
 }
-else {
-  document.onreadystatechange = function(){
-    if (document.readyState === 'complete'){
-      inject_payload()
-    }
-  }
+
+var inject_options_then_function = function(_function){
+  inject_options()
+  inject_function(_function)
+}
+
+if (user_options['open_in_webcast_reloaded'] || user_options['open_in_exoairplayer_sender']){
+  setTimeout(
+    function(){
+      inject_options_then_function(payload)
+    },
+    user_options['script_injection_delay_ms']
+  )
 }

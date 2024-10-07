9bdc220b265719af89ec93dc494b8a6a02429d59
Warren R. Bank <warren.r.bank@gmail.com>
2024-10-07 15:54:19 -0700

    v1.1.0: display video titles on selective host domains

--- webmonkey-userscript/Ok-ru-helpers.user.js
+++ webmonkey-userscript/Ok-ru-helpers.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ok.ru Helpers
 // @description  List all embedded videos.
-// @version      1.0.0
+// @version      1.1.0
 // @include      /^https?:\/\/(?:[^\.\/]*\.)*(?:nfl-video\.com)\/.*$/
 // @icon         https://ok.ru/favicon.ico
 // @run-at       document-end
@@ -22,9 +22,11 @@ var iframe_regex = new RegExp('[/\\.]ok\\.ru/video(?:embed)?/', 'i')
 // ----------------------------------------------------------------------------- process page
 
 var process_page = function() {
-  var iframes     = unsafeWindow.document.querySelectorAll('iframe[src]')
-  var iframe_urls = []
-  var iframe_url
+  var hostname      = unsafeWindow.location.hostname.toLowerCase()
+  var iframes       = unsafeWindow.document.querySelectorAll('iframe[src]')
+  var iframe_urls   = []
+  var iframe_titles = []
+  var iframe_url, iframe_title
 
   for (var i=0; i < iframes.length; i++) {
     iframe_url = iframes[i].getAttribute('src')
@@ -32,11 +34,24 @@ var process_page = function() {
     if (iframe_regex.test(iframe_url)) {
       iframe_url = resolve_url(iframe_url)
       iframe_urls.push(iframe_url)
+
+      iframe_title = null
+      try {
+        if (hostname.endsWith('nfl-video.com')) {
+          iframe_title = iframes[i].parentElement.previousElementSibling.querySelector('[title], strong')
+          iframe_title = iframe_title.hasAttribute('title') ? iframe_title.getAttribute('title') : iframe_title.textContent
+          iframe_title = iframe_title.trim()
+        }
+      }
+      catch(e) {
+        iframe_title = null
+      }
+      iframe_titles.push(iframe_title)
     }
   }
 
   if (iframe_urls.length)
-    rewrite_dom(iframe_urls)
+    rewrite_dom(iframe_urls, iframe_titles)
 }
 
 var resolve_url = function(url) {
@@ -52,7 +67,7 @@ var resolve_url = function(url) {
   return unsafeWindow.location.protocol + '//' + unsafeWindow.location.host + unsafeWindow.location.pathname.replace(/[^\/]+$/, '') + url
 }
 
-var rewrite_dom = function(iframe_urls) {
+var rewrite_dom = function(iframe_urls, iframe_titles) {
   unsafeWindow.document.body.innerHTML = `
 <style>
   body {padding: 20px; background-color: #ffffff;}
@@ -62,7 +77,7 @@ var rewrite_dom = function(iframe_urls) {
 </style>
 
 <ul>
-  ${iframe_urls.map(url => `<li><a target="_blank" href="${url}">${url}</a></li>`).join("\n  ")}
+  ${iframe_urls.map((url, index) => `<li><a target="_blank" href="${url}">${url}</a>${iframe_titles[index] ? `<div>${iframe_titles[index]}</div>` : ''}</li>`).join("\n  ")}
 </ul>
 `
 }
